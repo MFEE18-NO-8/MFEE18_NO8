544ec6f10acc11980ca221df2dfc697390e3ba9c
Mei Jun Chen <wdxlf8992@gmail.com>
2021-09-16 21:45:40 +0800

    毛孩知識

--- HomeForStray/app.js
+++ HomeForStray/app.js
@@ -22,6 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+// 引入 毛孩知識管理清單
+var KnowManageList = require('./routes/KnowManageList');
+app.use('/KnowManageList', KnowManageList);
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
