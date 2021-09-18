df97b634d00a48a81366663f1a83b52a64f347c9
lynn880703 <mm900919@gmail.com>
2021-09-18 18:04:03 +0800

    毛孩清單-撈資料

--- HomeForStray/routes/AdopList.js
+++ HomeForStray/routes/AdopList.js
@@ -4,7 +4,13 @@ var mysql =require('mysql'); //含入mysql套件
 var pool=require('./lib/db.js') //含入資料庫連線
 
 router.get('/', function (req, res, next) {
-    res.render('AdopList');
+
+    pool.query('select * from PostForAdopt order by PetId desc',function(err,results){
+        if(err) throw err;
+        res.render('AdopList', {Pet:results});
+    })
+
+
 });
 
 module.exports = router;
