d15987b665b296680c2bcbfd77fc3b5934d934db
MinYu <gaexp10a05965@gmail.com>
2021-09-29 21:29:36 +0800

    查詢+編輯更新

--- HomeForStray/routes/BackFosterManage.js
+++ HomeForStray/routes/BackFosterManage.js
@@ -113,62 +113,27 @@ router.post('/FosterManageAdd', function (req, res) {
 
 // 刊登審核後台編輯
 
-router.get('/ForsterManageEdit', function (req, res) {
+router.get('/FosterManageEdit', function (req, res) {
     var PetId = 0;
     PetId = req.query.PetId;  //取得傳送的資料id
     // var PetId = parseInt(req.query.PetId);
     pool.query('select * from postforadopt where PetId=?',
-        [
-            PetId
-        ],
-
+        [PetId],
         function (err, results) {  //根據PetId讀取資料
-
             if (err) throw err;
             res.render('FosterManageEdit', { data: results, PetId: PetId });
         });
 });
 
-router.post('/ForsterManageEdit', function (req, res) {
+router.post('/FosterManageEdit', function (req, res) {
     // 1. req取得PetId 
-
     var PetId = req.body.PetId
-    var PetName = req.body.PetName
-    var AdoptDate = req.body.AdoptDate
-    var PetSpecies = req.body.PetSpecies
-    var PetBreed = req.body.PetBreed
-    var BodyType = req.body.BodyType
-    var PetFur = req.body.PetFur
-    var PetAge = req.body.PetAge
-    var PetGender = req.body.PetGender
-    var Neuter = req.body.Neuter
-    var Microchip = req.body.Microchip
-    var ContactPerson = req.body.ContactPerson
-    var ContactPhone = req.body.ContactPhone
-    var PetDes = req.body.PetDes
-    // var AdoptSatae = req.body.AdoptSatae
-
+    var AdoptState = req.body.AdoptState
     // 2. 根據PetId將資料 
-    pool.query('UPDATE `postforadopt` SET ? WHERE PetId=? ',
-        [{
-            PetName,
-            AdoptDate,
-            PetSpecies,
-            PetBreed,
-            BodyType,
-            PetFur,
-            PetAge,
-            PetGender,
-            Neuter,
-            Microchip,
-            ContactPerson,
-            ContactPhone,
-            PetDes,
-            // AdoptSatae,
-        }, PetId],
-        function (err, results) {  //料
+    pool.query('UPDATE `postforadopt` SET AdoptState=? WHERE PetId=? ',
+        [AdoptState, PetId],
+        function (err, results) {
             if (err) throw err;
-
             // 3. 跳轉到List
             res.redirect('/BackFosterManage/FosterManageList');
         });
@@ -176,7 +141,7 @@ router.post('/ForsterManageEdit', function (req, res) {
 
 
 // 刊登審核後台刪除
-router.get('/ForsterManageDel', function (req, res) {
+router.get('/FosterManageDel', function (req, res) {
     var PetId = 0;
     PetId = req.query.PetId;  //取得傳送的資料id
     // var PetId = parseInt(req.query.PetId);
@@ -192,7 +157,7 @@ router.get('/ForsterManageDel', function (req, res) {
         });
 });
 
-router.post('/ForsterManageDel', function (req, res) {
+router.post('/FosterManageDel', function (req, res) {
     // 1. req取得PetId 
 
     var PetId = req.body.PetId
