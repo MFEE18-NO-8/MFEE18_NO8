9ee6ec58662d6cb52aa92ace24134edc00715cad
Mei Jun Chen <wdxlf8992@gmail.com>
2021-09-29 21:10:52 +0800

    使用者 更新js

--- HomeForStray/routes/UserFoster.js
+++ HomeForStray/routes/UserFoster.js
@@ -1,10 +1,13 @@
-var express =require('express');
-var router =express.Router();
-var mysql =require('mysql'); //含入mysql套件
-var pool=require('./lib/db.js') //含入資料庫連線
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql'); //含入mysql套件
+var pool = require('./lib/db.js') //含入資料庫連線
 
 router.get('/', function (req, res, next) {
-    res.render('UserFoster');
+    pool.query('select * from Member where Email=?', [req.session.Email], function (err, results) {
+        var memberData = results[0]; // 撈取是否有登入session
+        res.render('UserFoster', { memberData: memberData || "", });
+    });
 });
 
 module.exports = router;
