9152ebe61ba5a113e8fdcb52f06fd0d9db44eb8c
Mei Jun Chen <wdxlf8992@gmail.com>
2021-09-29 19:59:50 +0800

    追蹤 通知 JS Nav調整

--- HomeForStray/routes/UserFollow.js
+++ HomeForStray/routes/UserFollow.js
@@ -1,7 +1,7 @@
-var express =require('express');
-var router =express.Router();
-var mysql =require('mysql'); //含入mysql套件
-var pool=require('./lib/db.js') //含入資料庫連線
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql'); //含入mysql套件
+var pool = require('./lib/db.js') //含入資料庫連線
 //設定網頁 每頁資料筆數
 var LinePerPage = 5;
 
@@ -10,25 +10,29 @@ router.get('/', function (req, res, next) {
     if (isNaN(PageNo) || PageNo < 1) {  //如果沒有傳送參數,設目前頁數為第1頁
         PageNo = 1;
     }
-    pool.query('SELECT * FROM UserFollow JOIN member ON(member.MemberID=UserFollow.MemberID) JOIN PostForAdopt ON (PostForAdopt.Petid=UserFollow.Petid) WHERE UserFollowState = 1 AND Email=?',[req.session.Email], function(err, results){
-        var FollowData = results; // 撈取是否有登入session
-        console.log(FollowData)
+    pool.query('select * from Member where Email=?', [req.session.Email], function (err, results) {
+        var memberData = results[0]; // 撈取是否有登入session
+        pool.query('SELECT * FROM UserFollow JOIN member ON(member.MemberID=UserFollow.MemberID) JOIN PostForAdopt ON (PostForAdopt.Petid=UserFollow.Petid) WHERE UserFollowState = 1 AND Email=?', [req.session.Email], function (err, results) {
+            var FollowData = results; // 撈取是否有登入session
+            console.log(FollowData)
 
-        var TotalLine = FollowData.length; //資料總筆數 朱建輝 有兩筆追蹤資料
-        var TotalPage = Math.ceil(TotalLine / LinePerPage); //資料總頁數＝總筆數/每頁顯示數  朱建輝資料總頁數 1 ，因為總共2筆/每頁顯示5筆資料
+            var TotalLine = FollowData.length; //資料總筆數 朱建輝 有兩筆追蹤資料
+            var TotalPage = Math.ceil(TotalLine / LinePerPage); //資料總頁數＝總筆數/每頁顯示數  朱建輝資料總頁數 1 ，因為總共2筆/每頁顯示5筆資料
 
-        pool.query('SELECT * FROM UserFollow ORDER BY FollowDate DESC LIMIT ?,?',[(PageNo - 1) * LinePerPage, LinePerPage],
-        function(err,results){
-            if (err) throw err;
-            res.render('UserFollow', { 
-                data: results, 
-                FollowData: FollowData || "", 
-                PageNo: PageNo, 
-                TotalLine: TotalLine, 
-                TotalPage: TotalPage, 
-                LinePerPage: LinePerPage });
+            pool.query('SELECT * FROM UserFollow ORDER BY FollowDate DESC LIMIT ?,?', [(PageNo - 1) * LinePerPage, LinePerPage],
+                function (err, results) {
+                    if (err) throw err;
+                    res.render('UserFollow', {
+                        data: results,
+                        FollowData: FollowData || "",
+                        PageNo: PageNo,
+                        TotalLine: TotalLine,
+                        TotalPage: TotalPage,
+                        LinePerPage: LinePerPage,
+                        memberData: memberData || "",
+                    });
+                });
         });
-        
     });
 });
 
