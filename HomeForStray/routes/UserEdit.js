52291b43f71fb46b43af2952592e6bb4a1719a2f
Luca <86730662+lucaMottoHayaku@users.noreply.github.com>
2021-09-30 13:10:24 +0800

    UserEdit抓資料已完成

--- HomeForStray/routes/UserEdit.js
+++ HomeForStray/routes/UserEdit.js
@@ -1,21 +1,21 @@
-var express =require('express');
-var router =express.Router();
-var mysql =require('mysql'); //含入mysql套件
-var pool=require('./lib/db.js') //含入資料庫連線
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql'); //含入mysql套件
+var pool = require('./lib/db.js') //含入資料庫連線
 
 router.get('/', function (req, res, next) {
     pool.query('SELECT * from `member` WHERE `Email`=?', [req.session.Email], function (err, results) {
         var memberData = results[0]; // 撈取是否有登入session
-        pool.query('SELECT * FROM `member` WHERE `Email`=?',[req.session.Email], function (err, results){
-            var UserData =results;
+        pool.query('SELECT * FROM `member` WHERE `Email`=?', [req.session.Email], function (err, results) {
+            var UserData = results;
             console.log(UserData)
             if (err) throw err;
-            res.render('UserEdit', { 
-                UserData: UserData || "", 
+            res.render('UserEdit', {
+                UserData: UserData[0] || "",
                 memberData: memberData || "",
             });
         })
-        
+
     });
 });
 
