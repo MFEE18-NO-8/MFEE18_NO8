c31f3fc3287c6edf2b6532d9ee52900ae3c510fe
Mei Jun Chen <wdxlf8992@gmail.com>
2021-10-02 21:08:18 +0800

    查詢 小改

--- HomeForStray/routes/BackNewsManage.js
+++ HomeForStray/routes/BackNewsManage.js
@@ -3,8 +3,28 @@ var router = express.Router();
 var mysql = require('mysql');        //含入mysql套件
 var pool = require('./lib/db.js');   //含入資料庫連線
 
-// 毛孩新聞後台清單 查詢
+// 最新消息後台清單
 var linePerPage = 5;  // 每頁資料筆數
+router.get('/NewsManageList', function (req, res, next) {
+    var pageNo = parseInt(req.query.pageNo);  //取得傳送的目前頁數
+    if (isNaN(pageNo) || pageNo < 1) {  //如果沒有傳送參數,設目前頁數為第1頁
+        pageNo = 1;
+    }
+
+    pool.query('select count(*) as cnt from News', function (err, results) {  //讀取資料總筆數
+        if (err) throw err;
+        var totalLine = results[0].cnt;  //資料總筆數
+        var totalPage = Math.ceil(totalLine / linePerPage);  //總頁數
+
+        pool.query('select * from News order by NewsId desc limit ?, ?', [(pageNo - 1) * linePerPage, linePerPage], function (err, results) {  //根據目前頁數讀取資料
+            if (err) throw err;
+            res.render('NewsManageList', { data: results, pageNo: pageNo, totalLine: totalLine, totalPage: totalPage, linePerPage: linePerPage });
+        });
+    });
+});
+
+// 毛孩新聞後台清單 查詢
+
 router.post('/NewsManageList', function (req, res) {
     var NewsTitle = req.body.NewsTitle;
     var ShowStartDate = req.body.ShowStartDate;
@@ -31,6 +51,7 @@ router.post('/NewsManageList', function (req, res) {
         ],
         function (err, results) {  //讀取資料總筆數
             if (err) throw err;
+            var linePerPage = 100;  // 每頁資料筆數
             var totalLine = results[0].cnt;  //資料總筆數
             var totalPage = Math.ceil(totalLine / linePerPage);  //總頁數
             var pageNo = parseInt(req.query.pageNo);  //取得傳送的目前頁數
@@ -62,26 +83,6 @@ router.post('/NewsManageList', function (req, res) {
 
     });
 
-// 最新消息後台清單
-var linePerPage = 5;  // 每頁資料筆數
-
-router.get('/NewsManageList', function (req, res, next) {
-    var pageNo = parseInt(req.query.pageNo);  //取得傳送的目前頁數
-    if (isNaN(pageNo) || pageNo < 1) {  //如果沒有傳送參數,設目前頁數為第1頁
-        pageNo = 1;
-    }
-
-    pool.query('select count(*) as cnt from News', function (err, results) {  //讀取資料總筆數
-        if (err) throw err;
-        var totalLine = results[0].cnt;  //資料總筆數
-        var totalPage = Math.ceil(totalLine / linePerPage);  //總頁數
-
-        pool.query('select * from News order by NewsId desc limit ?, ?', [(pageNo - 1) * linePerPage, linePerPage], function (err, results) {  //根據目前頁數讀取資料
-            if (err) throw err;
-            res.render('NewsManageList', { data: results, pageNo: pageNo, totalLine: totalLine, totalPage: totalPage, linePerPage: linePerPage });
-        });
-    });
-});
 
 
 // 最新消息後台新增
