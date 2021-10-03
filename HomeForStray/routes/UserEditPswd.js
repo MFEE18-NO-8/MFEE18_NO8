539c0a15934debae51737a4615921d00f30b3296
Mei Jun Chen <wdxlf8992@gmail.com>
2021-10-03 23:30:02 +0800

    會員密碼變更 訊息框

--- HomeForStray/routes/UserEditPswd.js
+++ HomeForStray/routes/UserEditPswd.js
@@ -7,15 +7,16 @@ const crypto = require('crypto');
 
 router.get('/', function (req, res, next) {
     var messages = ""
+    var alert = ""
+
     pool.query('SELECT * from `member` WHERE `Email`=?', [req.session.Email], function (err, results) {
         var memberData = results[0]; // 撈取是否有登入session
         if (err) throw err;
         res.render('UserEditPswd', {
             memberData: memberData || "",
-            messages: messages
+            messages: messages,
+            alert: alert,
         });
-
-
     });
 });
 
@@ -34,20 +35,34 @@ router.post('/', function (req, res, next) {
         hashPassword.update(oldPswd);
         const oldPassword = hashPassword.digest('hex');
 
+        var alert = ""
+
         // 資料庫密碼 != 輸入密碼
         if (memberData.Password != oldPassword && memberData.Password != oldPswd) {
             messages = "舊密碼不正確"
-            res.render('UserEditPswd', { messages: messages, memberData: memberData || "", })
+            res.render('UserEditPswd', {
+                messages: messages,
+                memberData: memberData || "",
+                alert: true,
+            })
             return;
             // 輸入的新密碼 != 輸入的確認密碼
         } else if (newPswd !== newPswdConfirm) {
             messages = "新密碼不一致，請重新輸入"
-            res.render('UserEditPswd', { messages: messages, memberData: memberData || "", })
+            res.render('UserEditPswd', {
+                memberData: memberData || "",
+                alert: true,
+                messages: messages,
+            })
             return;
             // 資料庫密碼 = 輸入的新密碼
-        } else if (memberData.Password == newPswd){
+        } else if (memberData.Password == newPswd) {
             messages = "輸入的新舊密碼一致，請重新輸入"
-            res.render('UserEditPswd', { messages: messages, memberData: memberData || "", })
+            res.render('UserEditPswd', {
+                memberData: memberData || "",
+                alert: true,
+                messages: messages,
+            })
             return;
         }
 
@@ -55,7 +70,7 @@ router.post('/', function (req, res, next) {
         let hashPassword2 = crypto.createHash('sha1');
         hashPassword2.update(newPswd);
         const newPassword = hashPassword2.digest('hex');
-        
+
         ;//錯誤訊息
         pool.query('update member set ? where Email=?', [{  //更新資料
             Password: newPassword,
@@ -64,8 +79,10 @@ router.post('/', function (req, res, next) {
             var messages = ""
             messages = "密碼更新成功"
             res.render('UserEditPswd', {
-                messages: messages, 
-                memberData: memberData || "",});
+                messages: messages,
+                alert: false,
+                memberData: memberData || "",
+            });
         });
     });
 });
