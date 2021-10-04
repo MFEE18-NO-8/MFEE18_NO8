197b96122cc106f211db06c23b4ce5531aaf2cbe
Mei Jun Chen <wdxlf8992@gmail.com>
2021-10-04 22:25:56 +0800

    後台 審核刊登送養 查詢 更新

--- HomeForStray/routes/BackFosterManage.js
+++ HomeForStray/routes/BackFosterManage.js
@@ -4,13 +4,41 @@ var router = express.Router();
 var mysql = require('mysql');        //含入mysql套件
 var pool = require('./lib/db.js');   //含入資料庫連線
 
+//毛孩知識後台清單
+var LinePage = 5;  // 每頁資料筆數
 
-// 刊登審核後台清單
-router.get('/FosterManageList', function (req, res) {
-    var PetName = req.query.PetName
-    var FosterDateStart = req.query.FosterDateStart
-    var FosterDateEnd = req.query.FosterDateEnd
-    var AdoptState = req.query.AdoptState
+router.get('/FosterManageList', function (req, res, next) {
+    var PageNum = parseInt(req.query.PageNum);  //取得傳送的目前頁數
+    if (isNaN(PageNum) || PageNum < 1) {  //如果沒有傳送參數,設目前頁數為第1頁
+        PageNum = 1;
+    }
+
+    pool.query('select count(*) as cnt from postforadopt', function (err, results) {  //讀取資料總筆數
+        if (err) throw err;
+        var TotalLine = results[0].cnt;  //資料總筆數
+        var TotalPage = Math.ceil(TotalLine / LinePage);  //總頁數
+
+        pool.query('select * from postforadopt order by PetId desc limit ?, ?;',
+            [(PageNum - 1) * LinePage, LinePage],
+            function (err, results) {  //根據目前頁數讀取資料
+                if (err) throw err;
+                res.render('FosterManageList', {
+                    data: results,
+                    PageNum: PageNum,
+                    TotalLine: TotalLine,
+                    TotalPage: TotalPage,
+                    LinePage: LinePage
+                });
+            });
+    });
+});
+
+// 刊登審核後台清單 查詢
+router.post('/FosterManageList', function (req, res) {
+    var PetName = req.body.PetName
+    var FosterDateStart = req.body.FosterDateStart
+    var FosterDateEnd = req.body.FosterDateEnd
+    var AdoptState = req.body.AdoptState
 
     if (PetName === undefined) {
         PetName = ''
@@ -24,10 +52,10 @@ router.get('/FosterManageList', function (req, res) {
     if (AdoptState === undefined) {
         AdoptState = ''
     }
-
-    pool.query(" select  count(*) as cnt  from postforadopt WHERE (PetName=? OR ?='') AND (AdoptDate>=? OR ?='' ) AND (AdoptDate<=? OR ?='' ) AND (AdoptState=? OR ?='') ",
+    var sql1 = `select count(*) as cnt from postforadopt WHERE (PetName like '%${PetName}%' OR ?='')  AND (AdoptDate>=? OR ?='' ) AND (AdoptDate<=? OR ?='' ) AND (AdoptState=? OR ?='') `
+    pool.query(sql1,
         [
-            PetName,
+ 
             PetName,
             FosterDateStart,
             FosterDateStart,
@@ -38,7 +66,7 @@ router.get('/FosterManageList', function (req, res) {
         ],
         function (err, results) {  //讀取資料總筆數
             if (err) throw err;
-            var LinePage = 5;
+            var LinePage = 100; //每頁筆數
             var TotalLine = results[0].cnt;  //資料總筆數
             var TotalPage = Math.ceil(TotalLine / LinePage);  //總頁數 補充: Math.ceil 讓多一筆資料直接變成新一頁放置其中
             var PageNum = parseInt(req.query.PageNum);  //取得傳送的目前頁數 (字串轉成整數如果第一個字串無法被解析為任何數字，parseInt 會回傳 NaN)
@@ -49,10 +77,9 @@ router.get('/FosterManageList', function (req, res) {
 
 
 
-
-            pool.query(" select * from postforadopt WHERE (PetName=? OR ?='') AND (AdoptDate>=? OR ?='' ) AND (AdoptDate<=? OR ?='' ) AND (AdoptState=? OR ?='' ) order by  AdoptDate , PetId  limit ?, ? ", // 選此資料表 用PetId排序
+            var sql2 = `select * from postforadopt WHERE (PetName like '%${PetName}%'  OR  ?='') AND (AdoptDate>=? OR ?='' ) AND (AdoptDate<=? OR ?='' ) AND (AdoptState=? OR ?='' ) order by  AdoptDate , PetId  limit ?, ?  `
+            pool.query(sql2, // 選此資料表 用PetId排序
                 [
-                    PetName,
                     PetName,
                     FosterDateStart,
                     FosterDateStart,
@@ -70,7 +97,6 @@ router.get('/FosterManageList', function (req, res) {
                         {
                             data: results,
                             PageNum: PageNum,
-                            req: req,
                             TotalLine: TotalLine,
                             TotalPage: TotalPage,
                             LinePage: LinePage
@@ -79,6 +105,82 @@ router.get('/FosterManageList', function (req, res) {
         })
 
 });
+
+
+// // 刊登審核後台清單
+// router.get('/FosterManageList', function (req, res) {
+//     var PetName = req.query.PetName
+//     var FosterDateStart = req.query.FosterDateStart
+//     var FosterDateEnd = req.query.FosterDateEnd
+//     var AdoptState = req.query.AdoptState
+
+//     if (PetName === undefined) {
+//         PetName = ''
+//     }
+//     if (FosterDateStart === undefined) {
+//         FosterDateStart = ''
+//     }
+//     if (FosterDateEnd === undefined) {
+//         FosterDateEnd = ''
+//     }
+//     if (AdoptState === undefined) {
+//         AdoptState = ''
+//     }
+
+//     pool.query(" select  count(*) as cnt  from postforadopt WHERE (PetName=? OR ?='') AND (AdoptDate>=? OR ?='' ) AND (AdoptDate<=? OR ?='' ) AND (AdoptState=? OR ?='') ",
+//         [
+//             PetName,
+//             PetName,
+//             FosterDateStart,
+//             FosterDateStart,
+//             FosterDateEnd,
+//             FosterDateEnd,
+//             AdoptState,
+//             AdoptState
+//         ],
+//         function (err, results) {  //讀取資料總筆數
+//             if (err) throw err;
+//             var LinePage = 5;
+//             var TotalLine = results[0].cnt;  //資料總筆數
+//             var TotalPage = Math.ceil(TotalLine / LinePage);  //總頁數 補充: Math.ceil 讓多一筆資料直接變成新一頁放置其中
+//             var PageNum = parseInt(req.query.PageNum);  //取得傳送的目前頁數 (字串轉成整數如果第一個字串無法被解析為任何數字，parseInt 會回傳 NaN)
+//             if (isNaN(PageNum) || PageNum < 1)          //如果沒有傳送參數,設目前頁數為第1頁 || 是 or 的意思
+//             {
+//                 PageNum = 1;
+//             }
+
+
+
+
+//             pool.query(" select * from postforadopt WHERE (PetName=? OR ?='') AND (AdoptDate>=? OR ?='' ) AND (AdoptDate<=? OR ?='' ) AND (AdoptState=? OR ?='' ) order by  AdoptDate , PetId  limit ?, ? ", // 選此資料表 用PetId排序
+//                 [
+//                     PetName,
+//                     PetName,
+//                     FosterDateStart,
+//                     FosterDateStart,
+//                     FosterDateEnd,
+//                     FosterDateEnd,
+//                     AdoptState,
+//                     AdoptState,
+
+//                     (PageNum - 1) * LinePage,
+//                     LinePage
+//                 ],
+//                 function (err, results) {  //根據目前頁數讀取資料  )
+//                     if (err) throw err;
+//                     res.render('FosterManageList',  //丟到 ejs 模板上
+//                         {
+//                             data: results,
+//                             PageNum: PageNum,
+//                             req: req,
+//                             TotalLine: TotalLine,
+//                             TotalPage: TotalPage,
+//                             LinePage: LinePage
+//                         });
+//                 });
+//         })
+
+// });
 
 
 // 刊登審核後台新增
