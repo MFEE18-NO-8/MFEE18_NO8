5141b933b83c621a693df1b0ce524008d11b6a7b
Mei Jun Chen <wdxlf8992@gmail.com>
2021-10-04 20:54:12 +0800

    毛孩知識

--- HomeForStray/routes/BackKnowManage.js
+++ HomeForStray/routes/BackKnowManage.js
@@ -97,17 +97,14 @@ router.get('/KnowManageAdd', function (req, res, next) {
 router.post('/KnowManageAdd', function (req, res, next) {
     console.log(req.file);
     var pageNo = parseInt(req.query.pageNo);
-    var articleTitle = req.body['articleTitle'];  //取得輸入的類型
-    var articleDate = req.body['articleDate'];
-    var articleContent = req.body['articleContent'];
+    var articleTitle = req.body['ArticleTitle'];  //取得輸入的類型
+    var articleDate = req.body['ArticleDate'];
+    var articleContent = req.body['ArticleContent'];
 
-
-
-    pool.query('insert into articlenews set ?', [{  //新增資料
+    pool.query('insert into articlenews set ?', [{//新增資料
         ArticleTitle: articleTitle,
         ArticleDate: articleDate,
         ArticleCont: articleContent,
-
 
     }], function (err, results) {
         if (err) throw err;
@@ -199,9 +196,9 @@ router.get('/KnowManageEdit', function (req, res, next) {
 
 router.post('/KnowManageEdit', function (req, res, next) {
     var pageNo = parseInt(req.query.pageNo);
-    var articleTitle = req.body['articleTitle'];  //取得輸入的類型
-    var articleDate = req.body['articleDate'];
-    var articleContent = req.body['articleContent'];
+    var articleTitle = req.body['ArticleTitle'];  //取得輸入的類型
+    var articleDate = req.body['ArticleDate'];
+    var articleContent = req.body['ArticleContent'];
 
 
     pool.query('update articlenews set ? where ArticleId=?', [{  //更新資料
