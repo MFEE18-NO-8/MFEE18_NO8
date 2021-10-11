c5fc4e103d91315db49ca645ae8d9912a9c8eacc
MinYu <gaexp10a05965@gmail.com>
2021-10-11 14:52:28 +0800

    上傳功能

--- HomeForStray/routes/AdoptList.js
+++ HomeForStray/routes/AdoptList.js
@@ -46,7 +46,7 @@ router.get('/', function (req, res) {
 
 
                 // pool.query(" select * from postforadopt, CityDatas, PetImgDatas WHERE ((PostForAdopt.CityId=CityDatas.CityId)=? OR ?='') AND (PetGender=? OR ?='' ) AND (PetSpecies=? OR ?='') order by  AdoptDate DESC  limit ?, ? ", // 選此資料表 用PetId排序
-                pool.query(" select * from postforadopt INNER JOIN CityDatas ON PostForAdopt.CityId = CityDatas.CityId INNER JOIN PetImgDatas ON PostForAdopt.PetId = PetImgDatas.PetId WHERE (PostForAdopt.CityId=? OR ?='') AND (PetGender=? OR ?='' ) AND (PetSpecies=? OR ?='') order by  AdoptDate DESC  limit ?, ? ", // 選此資料表 用PetId排序
+                pool.query(" select * from postforadopt INNER JOIN CityDatas ON PostForAdopt.CityId = CityDatas.CityId INNER JOIN PetImgDatas ON PostForAdopt.PetImgId = PetImgDatas.PetImgId WHERE (PostForAdopt.CityId=? OR ?='') AND (PetGender=? OR ?='' ) AND (PetSpecies=? OR ?='') order by  AdoptDate DESC  limit ?, ? ", // 選此資料表 用PetId排序
                     [
                         CityId,
                         CityId,
@@ -78,68 +78,6 @@ router.get('/', function (req, res) {
 
 })
 
-
-
-
-
-
-
-//     var pageNo = parseInt(req.query.pageNo);  //取得傳送的目前頁數
-//     if (isNaN(pageNo) || pageNo < 1) {  //如果沒有傳送參數,設目前頁數為第1頁
-//         pageNo = 1;
-//     }
-//     pool.query('select * from Member where Email=?', [req.session.Email], function (err, results) {
-//         var memberData = results[0]; // 撈取是否有登入session
-
-
-
-//         pool.query('select count(*) as cnt from PostForAdopt', function (err, results) {  //讀取資料總筆數
-//             if (err) throw err;
-//             var totalLine = results[0].cnt;  //資料總筆數
-//             var totalPage = Math.ceil(totalLine / linePerPage);  //總頁數
-
-//             pool.query('select * from PostForAdopt,CityDatas,PetImgDatas where  PostForAdopt.CityId=CityDatas.CityId and PostForAdopt.PetId=PetImgDatas.PetId and PostForAdopt.PetImgId=PetImgDatas.PetImgId order by PostForAdopt.PetId asc limit ?, ?', [(pageNo - 1) * linePerPage, linePerPage], function (err, results) {  //根據目前頁數讀取資料
-//                 if (err) throw err;
-//                 res.render('AdoptList', 
-//                 {   data: results, 
-//                     memberData: memberData || "",
-//                     pageNo: pageNo, 
-//                     totalLine: totalLine, 
-//                     totalPage: totalPage, 
-//                     linePerPage: linePerPage });
-//             });
-//         });
-//     });
-// });
-
-// router.post('/', function (req, res) {  // app.js 已掛好路徑 post & get 會成對 >>
-//     // 1. 取出傳入的參數 
-
-//     // 2. 把參數丟到資料庫查詢
-//     pool.query("select * from PostForAdopt,CityDatas,PetImgDatas where (CityName=? OR ?='') AND (PetGender=? OR ?='' ) AND (PetSpecies=? OR ?='' )",
-//         // MySQL 能單個欄位查詢
-
-//         [
-//             req.body.CityName,
-//             req.body.CityName,
-//             req.body.PetGender,
-//             req.body.PetGender,
-//             req.body.PetSpecies,
-//             req.body.PetSpecies,
-//         ],
-
-//         function (err, results) {
-//             if (err) throw err;
-
-
-//             // 3. 把查詢結果作為參數傳給render
-//             res.render('AdoptList', { data: results, });
-
-//         })
-
-//     // res.render('FosterManageList', { PetName: req.body.PetName }); // req.params找網址 req.query 找?後面參數 req.body 表單
-
-// });
 
 
 module.exports = router;
